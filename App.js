0d5b965347e4c1e1c0143a2873013b10e94c985c
Sachin Arora <sachinarora0986@gmail.com>
2024-05-23 09:37:29 +0530

    base of the app is created 😀

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
+import { header } from "express/lib/request";
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement(
-    "div", {id : "parent"}, 
-     [
-     React.createElement("div", {id : "child"}, 
-     [React.createElement("h1", {}, "this is a h1 tag..!"),
-     React.createElement("h2", {}, "this is a h2 tag..!"),]), // multiple members or siblings are passed in the list or array.
-        React.createElement("div", {id : "child"}, 
-     [React.createElement("h1", {}, "this is a h1 tag of child 2..!"),
-        React.createElement("h2", {}, "this is a h2 tag of child 2..!"),
-    ]),
-    ]);
-const root = ReactDOM.createRoot(document.getElementById("sachin"));
-root.render(parent); //helps to put react in the html page .
+// React.createElement => Object => HTMLElement(render)
+ /*this is not user or Devloper friendly so we use JSX 
+for making it easy to write or understand */
+
+//root.render(heading);
+
+//JSX is a HTML like syntax it is not a HTML & JSX is just a syntax
+
+const Heading = () => (
+    <h1>This is a functional Component</h1>
+)
+
+const number = 10000;
+
+
+
+const AlertComponent = () => {
+    {number}
+    alert("this is a alert message...1");  
+}
+
+const message = React.createElement("div", {}, React.createElement("button", {onClick : AlertComponent}, "Show Alert"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render(<Heading />)
+
